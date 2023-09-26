aba69da83c6456c54ba0fa2cd9b4e88f0f85cf8e
vijoh01 <76432491+vijoh01@users.noreply.github.com>
2023-09-26 16:11:31 +0200

    Newss

--- components/Home.tsx
+++ components/Home.tsx
@@ -40,7 +40,7 @@ export function Home() {
           <div id="target-element" className="min-h-[22.6rem] max-h-[22.6rem] md:max-h-[22.6rem] lg:max-h-[22.6rem] md:min-h-[25rem] md:w-[28rem] lg:min-h-[31.5rem] lg:w-[34rem] md:translate-x-[22rem] lg:translate-x-[27.5rem] flex justify-center items-center overflow-hidden">
             
             <svg
-              className='animate-color dark:animate-color translate-x-48 md:translate-x-[14.2rem] lg:translate-x-[17.5rem] rotate-6 min-w-[13rem] w-[13rem]  scale-[2] md:scale-[2.5] lg:scale-[2.7] translate-y-[4.7rem]'
+              className='animate-color dark:animate-color translate-x-48 md:translate-x-[14.2rem] lg:translate-x-[17.5rem] rotate-6 min-w-[13rem] w-[13rem]  scale-[2] md:scale-[2.5] lg:scale-[3] translate-y-[4.7rem]'
               width="200"
               height="200" 
             >
@@ -56,7 +56,7 @@ export function Home() {
               <path  className="fill-current filter brightness-[0.4] dark:brightness-100 " fill="url(#gradient)" d="M52,22.09a29.27,29.27,0,0,1,8.69-4.59c7.26-2.4,15-2.25,22.57-2.09,21,.43,43.79,1.56,59.95,16.13,8,7.25,13.57,17.16,19,26.87Q174,79.66,185.77,100.93c2.36,4.25,4.76,8.64,5.59,13.53,1.18,7-1.05,14.2-4.83,19.94s-9,10.19-14.42,14.12c-18.13,13.23-39.38,21.9-61.2,22.72S66.73,164.62,50.29,149c-10.18-9.66-17.83-22-25.08-34.42-6.74-11.47-13.44-23.73-14.09-37.3-.68-14.66,6.08-29,16-39A75.74,75.74,0,0,1,52,22.09Z" />
             </svg>
    
-            <Image  className='-translate-x-28 contrast-[1.05] pointer-events-none filter grayscale-[0.2] dark:grayscale-[0.1] brightness-125 dark:brightness-125 min-w-[24rem] w-[24rem] md:min-w-[28rem] lg:min-w-[35rem] lg:scale-100 md:lg:translate-y-[10rem] translate-y-28' src={viktor} alt='dasd'></Image>
+            <Image  className='-translate-x-28 contrast-[1.05] pointer-events-none filter grayscale-[0.3] dark:grayscale-[0.3] brightness-125 dark:brightness-125 min-w-[24rem] w-[24rem] md:min-w-[28rem] lg:min-w-[35rem] lg:scale-100 md:lg:translate-y-[8.7rem] translate-y-28' src={viktor} alt='dasd'></Image>
          
           </div>
         </motion.div>
