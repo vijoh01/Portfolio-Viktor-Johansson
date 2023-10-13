199898491e974d3dd5c9c378fff27a125ec9c165
vijoh01 <76432491+vijoh01@users.noreply.github.com>
2023-10-13 17:23:36 +0200

    better version

--- components/Home.tsx
+++ components/Home.tsx
@@ -1,6 +1,6 @@
 "use client"
 import Image from 'next/image'
-import viktor from '@/imgs/jagfaked.png'
+import viktor from '@/imgs/nobac.png'
 import stars from '@/imgs/stars.jpg'
 import vector from '@/imgs/vector.jpg'
 import { BiDownArrowAlt } from 'react-icons/bi'
@@ -27,7 +27,7 @@ export function Home() {
       <div className='flex items-center h-[95%] w-full md:justify-center'>
         <motion.div initial="hide"
         whileInView="show"
-        exit="hide" variants={introPictureVariants} className='flex flex-col-reverse justify-between w-full pt-[2rem] md:flex-row md:w-[50rem] lg:w-[60rem]'>
+        exit="hide" variants={introPictureVariants} className='flex flex-col-reverse justify-between w-full pt-[2rem] md:flex-row md:w-[50rem] lg:w-[65rem]'>
         
           <div className='border-t-2 text-secondary-400 dark:text-secondary-500 md:border-none md:absolute text-center md:text-start flex flex-col self-center w-[24rem] md:w-[26rem] lg:w-[33rem] z-10'>
             
@@ -37,10 +37,10 @@ export function Home() {
 
           </div>
 
-          <div id="target-element" className="min-h-[22.6rem] max-h-[22.6rem] md:max-h-[22.6rem] lg:max-h-[22.6rem] md:min-h-[25rem] md:w-[28rem] lg:min-h-[31.5rem] lg:w-[34rem] md:translate-x-[22rem] lg:translate-x-[27.5rem] flex justify-center items-center overflow-hidden">
+          <div id="target-element" className="min-h-[22.6rem] max-h-[22.6rem] md:max-h-[22.6rem] lg:max-h-[30rem] md:min-h-[40rem] md:w-[28rem] lg:min-h-[40.5rem] lg:w-[40rem] md:translate-x-[22rem] lg:translate-x-[27rem] flex justify-center items-center overflow-hidden">
             
             <svg
-              className='animate-color dark:animate-color translate-x-48 md:translate-x-[14.2rem] lg:translate-x-[17.5rem] rotate-6 min-w-[13rem] w-[13rem]  scale-[2] md:scale-[2.5] lg:scale-[3] translate-y-[4.7rem]'
+              className='animate-color dark:animate-color translate-x-48 md:translate-x-[14.2rem] lg:translate-x-[17.5rem] rotate-6 min-w-[13rem] w-[13rem]  scale-[2] md:scale-[2.5] lg:scale-[3.4] md:translate-y-[4.7rem] translate-y-[4.7rem]'
               width="200"
               height="200" 
             >
@@ -56,14 +56,11 @@ export function Home() {
               <path  className="fill-current filter brightness-[0.4] dark:brightness-100 " fill="url(#gradient)" d="M52,22.09a29.27,29.27,0,0,1,8.69-4.59c7.26-2.4,15-2.25,22.57-2.09,21,.43,43.79,1.56,59.95,16.13,8,7.25,13.57,17.16,19,26.87Q174,79.66,185.77,100.93c2.36,4.25,4.76,8.64,5.59,13.53,1.18,7-1.05,14.2-4.83,19.94s-9,10.19-14.42,14.12c-18.13,13.23-39.38,21.9-61.2,22.72S66.73,164.62,50.29,149c-10.18-9.66-17.83-22-25.08-34.42-6.74-11.47-13.44-23.73-14.09-37.3-.68-14.66,6.08-29,16-39A75.74,75.74,0,0,1,52,22.09Z" />
             </svg>
    
-            <Image  className='-translate-x-28 contrast-[1.05] pointer-events-none filter grayscale-[0.3] dark:grayscale-[0.3] brightness-125 dark:brightness-125 min-w-[24rem] w-[24rem] md:min-w-[28rem] lg:min-w-[35rem] lg:scale-100 md:lg:translate-y-[8.7rem] translate-y-28' src={viktor} alt='dasd'></Image>
+            <Image  className='-translate-x-28 contrast-[1.05] pointer-events-none filter grayscale-[1] brightness-[1.2] dark:brightness-[1.2] min-w-[24rem] w-[24rem] md:min-w-[28rem] lg:min-w-[35rem] lg:scale-150 -translate-y-10 scale-[1.15] md:scale-[1.4] md:-translate-y-14  lg:-translate-y-[3.53rem]' src={viktor} alt='dasd'></Image>
          
           </div>
         </motion.div>
       </div>
-      
-   
-      
     </div>
   )
 }
--- pages/api/download.js
+++ pages/api/download.js
@@ -5,14 +5,14 @@ import { resolve } from 'path';
 export default async function handler(req, res) {
     console.log("yes");
     try {
-        const filename = './CV.pdf'; // Replace 'your-file.pdf' with the actual name of your PDF file
+        const filename = './Viktor_Johansson_CV.pdf'; // Replace 'your-file.pdf' with the actual name of your PDF file
 
     // Set response headers to trigger PDF download
     res.setHeader('Content-Disposition', `attachment; filename=${filename}`);
     res.setHeader('Content-Type', 'application/pdf');
 
     // Resolve the file path correctly
-    const filePath = resolve(process.cwd(), 'CV.pdf'); // Replace 'your-file.pdf' with the actual path to your PDF file
+    const filePath = resolve(process.cwd(), 'Viktor_Johansson_CV.pdf'); // Replace 'your-file.pdf' with the actual path to your PDF file
     const fileStream = fs.createReadStream(filePath);
         fileStream.pipe(res);
       } catch (error) {
